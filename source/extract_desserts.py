6b0fbc93fae8dc7982c520dd11d25dc049d05e9e
Angel Manzur <angel.manzur@gmail.com>
2019-07-17 16:58:30 -0400

    added to .gitignore the emacs backup files ~, and added py file to extract desserts fro the raw data based on the words in the title" "

---
+++ source/extract_desserts.py
@@ -0,0 +1,74 @@
+import json
+import numpy as np
+
+def get_raw_data(filename='sample_layer1.json'):
+    """
+    Open the json file, and load the data as a list
+    
+    Input:
+        filename, default value contains 250 entries
+    Output:
+        list with the recipes
+    """
+    data_dir = '../data/'
+    file_path = data_dir + filename
+    print('Loadin file ', file_path)
+    with open(file_path,'r') as file:
+        raw_data = json.load(file)
+
+    return raw_data
+
+
+def get_sweet_vocabulary():
+    """
+    Get a list of words associated with desserts
+    Input:
+
+    Output:
+        List of words identifying desserts
+    """
+    dessert_identifier = ['cookies','pie','crumble', 'cake', 'ice-cream', 
+                      'ice cream','praline','macaron','cheesecake','cupcake', 
+                      'waffle','pudding','truffle','toffee','tart',
+                      'torte', 'zabiglione','trifle','toffee','sweets',
+                      'sundae','strudel','sugar','shortcake','souffle',
+                      'shortbread','sherbet','scone'"s'mores",'bread',
+                      'popsicle','popover','brittle','pastry','parfait',
+                      'panna','cotta','nougat','muffin','mousse','meringue'
+                      'marshmallow','macaroon','ladyfingers','tiramisu','jelly',
+                      'icing','jellyroll','fudge', 'honey','gelato',
+                      'gelatin','gingersnaps','gingerbread','frosting','fritter',
+                      'flan','eclair','donut','doughnut','dessert','pastry',
+                      'custard','crepe','cobbler','churro','caramel',
+                      'cannoli','butterscotch','brownie','buttercream','bombe',
+                      'biscotti','baklava','Alaska','cream','ice','crisp',
+                      'chocolate','compote','confection','fondant','fro-yo',
+                      'ganache','icing','jell-o','marzipan','molasses',
+                      'melba','snow','sorbet','sweets','syrup','whip',
+                      'jello','gateau','glacee','gelatine','tarte','bonbon',
+                      'biscuits', 'blondie','buckeye','confection','candy',
+                      'cupcone','hostess','oreo','snickerdoodles','twinkie',
+                      'butter','parfaits','vanilla','leches'
+                     ]
+    
+    return dessert_identifier
+
+def find_desserts(all_recipes):
+    """
+    Extract the dessert recipes from a list of recipes
+    bsed on a list of identifier words
+    Input:
+        List of recipes
+    Output:
+        List of dessert recipes
+    """
+
+    dessert_list = []
+    dessert_ids = get_sweet_vocabulary()
+    for recipe in all_recipes:
+        recipe_title = recipe['title'].lower().split()
+        to_dessert_list = [word for word in recipe_title if word in dessert_ids]
+        if len(to_dessert_list)>0:
+            dessert_list.append(recipe)
+
+    return dessert_list
